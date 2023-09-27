c76074fcc172cd2733e30d7faeea87b2079acc0b
Familyggaminggggggggggggg <FamilysAssetSales@outlook.com>
2023-09-27 07:33:45 +0100

    indentations

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.use("/people", usersRoutes);
-app.get("/", (req, res) => res.send("Welcome to the Users API!"));
-app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+app.use("/users", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the API!"));
+app.all("*", (req, res) =>
+  res.send("You've tried reaching a route that doesn't exist.")
+);
 
-app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
